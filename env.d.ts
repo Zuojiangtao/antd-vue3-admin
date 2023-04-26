8510ad2be1a1c4941746983e22a982d5d3b5b4a4
ZuoJiangtao <1799158837@qq.com>
2023-04-26 14:34:42 +0800

    build: add env file and config env.d.ts

--- env.d.ts
+++ env.d.ts
@@ -1,1 +1,9 @@
 /// <reference types="vite/client" />
+
+interface ImportMetaNodeEnv {
+  readonly VITE_APP_ENV: string;
+}
+
+interface ImportMetaBaseUrl {
+  readonly VITE_APP_BASE_URL: string;
+}
