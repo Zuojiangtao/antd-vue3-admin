50a585bd83e9f481c5d24402f2f08d688cda0679
ZuoJiangtao <1799158837@qq.com>
2023-04-26 14:09:29 +0800

    build: ant-design-vue
    相关配置及插件

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,42 @@
-import { fileURLToPath, URL } from 'node:url'
+import { fileURLToPath, URL } from 'node:url';
 
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import vueJsx from '@vitejs/plugin-vue-jsx'
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import vueJsx from '@vitejs/plugin-vue-jsx';
+import Components from 'unplugin-vue-components/vite';
+import { AntDesignVueResolver } from 'unplugin-vue-components/resolvers';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), vueJsx()],
+  plugins: [
+    vue(),
+    vueJsx(),
+    // https://github.com/antfu/unplugin-vue-components
+    Components({
+      resolvers: [
+        AntDesignVueResolver({
+          importStyle: 'less',
+          resolveIcons: true,
+        }),
+      ],
+      dts: 'types/components.d.ts',
+    }),
+  ],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  }
-})
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+    },
+  },
+  // 处理ant-design-vue 样式文件
+  css: {
+    preprocessorOptions: {
+      less: {
+        javascriptEnabled: true,
+      },
+    },
+  },
+  // 依赖优化 - 预构建
+  optimizeDeps: {
+    include: ['vue', 'pinia', 'vue-router', 'ant-design-vue/es'],
+  },
+});
