215df1d5e37e9eec460fb5b326da91ce3bcfde99
ZuoJiangtao <1799158837@qq.com>
2023-09-18 14:05:53 +0800

    chore: env环境变量配置及相关类型定义更改

--- types/global.d.ts
+++ types/global.d.ts
@@ -1,20 +1,20 @@
+export {};
+
 declare global {
   interface ViteEnv {
-    VITE_PORT: number;
-    VITE_USE_MOCK: boolean;
-    VITE_USE_PWA: boolean;
-    VITE_USE_CDN: boolean;
-    VITE_USE_IMAGEMIN: boolean;
-    VITE_PUBLIC_PATH: string;
-    VITE_DROP_CONSOLE: boolean;
-    VITE_PROXY: [string, string][];
-    VITE_GLOB_APP_TITLE: string;
-    VITE_GLOB_APP_SHORT_NAME: string;
-    VITE_BUILD_COMPRESS: 'gzip' | 'brotli' | 'none';
-    VITE_BUILD_COMPRESS_DELETE_ORIGIN_FILE: boolean;
-  }
-
-  interface ImportMetaEnv extends ViteEnv {
-    __: unknown;
+    VITE_APP_NODE_ENV?: string;
+    VITE_PORT?: number;
+    VITE_USE_MOCK?: boolean;
+    VITE_USE_PWA?: boolean;
+    VITE_USE_CDN?: boolean;
+    VITE_USE_IMAGEMIN?: boolean;
+    VITE_DROP_CONSOLE?: boolean;
+    VITE_PROXY?: [string, string][];
+    VITE_GLOB_APP_TITLE?: string;
+    VITE_GLOB_APP_SHORT_NAME?: string;
+    VITE_GLOB_API_URL?: string;
+    VITE_GLOB_API_URL_PREFIX?: string;
+    VITE_BUILD_COMPRESS?: 'gzip' | 'brotli' | 'none';
+    VITE_BUILD_COMPRESS_DELETE_ORIGIN_FILE?: boolean;
   }
 }
